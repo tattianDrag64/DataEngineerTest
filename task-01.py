4e51f8df3c7e8b61f316b64f63b934e9da09d1bf
tattianDrag64 <stu2301321089@uni-plovdiv.bg>
2024-12-22 23:36:48 +0200

    Identifies and handles any missing values in the dataset

--- task-01.py
+++ task-01.py
@@ -24,45 +24,53 @@ iris_dataset = pd.read_csv(url, header=None, names=table_col)
 #   + Summary statistics for each feature (mean, median, standard deviation).
 
 
-print("First 5 rows of the dataset:")
-print(iris_dataset.head())
+# print("First 5 rows of the dataset:")
+# print(iris_dataset.head())
 
-print("\nStatistics for num colums:")
-print(iris_dataset.describe())
+# print("\nStatistics for num colums:")
+# print(iris_dataset.describe())
 
-mean_values = iris_dataset.iloc[:, :-1].mean()
-std_values = iris_dataset.iloc[:, :-1].std()
+# mean_values = iris_dataset.iloc[:, :-1].mean()
+# std_values = iris_dataset.iloc[:, :-1].std()
 
-print("\nMean values for numeric columns:")
-print(mean_values)
+# print("\nMean values for numeric columns:")
+# print(mean_values)
 
-print("\nStandard deviation for numeric columns:")
-print(std_values)
+# print("\nStandard deviation for numeric columns:")
+# print(std_values)
 
-median_values = iris_dataset.iloc[:, :-1].median()
+# median_values = iris_dataset.iloc[:, :-1].median()
 
-print("\nMedian values for numeric columns:")
-print(median_values)
+# print("\nMedian values for numeric columns:")
+# print(median_values)
 
-# The count of unique values in the "species" column.
+# # The count of unique values in the "species" column.
 
-print("\nNumber of unique values in the 'species' column:")
-print(iris_dataset['species'].value_counts())
+# print("\nNumber of unique values in the 'species' column:")
+# print(iris_dataset['species'].value_counts())
 
 #   Generates a histogram for one numeric feature and a scatter plot comparing two features.
 
 import matplotlib.pyplot as plt
 import seaborn as sns 
 
-sns.histplot(iris_dataset['sepal_length'], kde = True)
-plt.title('Histogram of Sepal Length')
-plt.show()
+# sns.histplot(iris_dataset['sepal_length'], kde = True)
+# plt.title('Histogram of Sepal Length')
+# plt.show()
 
-sns.scatterplot(x='sepal_length', y='petal_length', hue='species', data=iris_dataset)
-plt.title('Scatter Plot: Sepal Length vs Petal Length')
-plt.show()
+# sns.scatterplot(x='sepal_length', y='petal_length', hue='species', data=iris_dataset)
+# plt.title('Scatter Plot: Sepal Length vs Petal Length')
+# plt.show()
 
+# Identifies and handles any missing values in the dataset (even if simulated by altering the dataset).
 
+iris_dataset.loc[0, 'sepal_length'] = np.nan
+print("\nDataset with a simulated missing value:")
+print(iris_dataset.head())
+
+iris_dataset['sepal_length'].fillna(iris_dataset['sepal_length'].mean(), inplace=True)
+print("\nDataset after handling missing values:")
+print(iris_dataset.head())
 
 
     
